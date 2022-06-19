c6e3eaeaefb733cb910c97c6fb69656382b28e8e
william <william@magicmanager.nl>
2022-06-19 23:39:58 +0200

    Executing a Pipeline

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -4,6 +4,7 @@ import {serialize} from "$services/queries/items/serialize";
 import {client} from "$services/redis";
 import {itemsKey} from "$services/keys";
 import {deserialize} from "$services/queries/items/deserialize";
+import {destroy_each} from "svelte/internal";
 
 export const getItem = async (id: string) => {
     const item = await client.hGetAll(itemsKey(id))
@@ -15,7 +16,17 @@ export const getItem = async (id: string) => {
 };
 
 export const getItems = async (ids: string[]) => {
+    const commands = ids.map((id) => {
+        return client.hGetAll(itemsKey(id))
+    })
+    const results = await Promise.all(commands)
 
+    return results.map((result, idx) => {
+        if (Object.keys(result).length === 0) {
+            return null
+        }
+        return deserialize(ids[idx], result)
+    })
 };
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
