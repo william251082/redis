71eebce06533c4371d3f69e32012911aedc0a2db
william <william@magicmanager.nl>
2022-06-19 14:32:55 +0200

    Hash commands

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,15 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('car', {
+        color: 'red',
+        year: 1950
+    })
+    const car = await client.hGetAll('car')
+    if (Object.keys(car).length === 0) {
+        console.log('Car not found, respond 404')
+    }
+    console.log(car)
+}
 run();
