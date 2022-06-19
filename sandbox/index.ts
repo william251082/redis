b857897f0604033f34153d4c4e8863cd79d8385e
william <william@magicmanager.nl>
2022-06-19 23:31:59 +0200

    Running Multiple Commands at the Same Time

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,14 +2,27 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-    await client.hSet('car', {
+    await client.hSet('car#1', {
         color: 'red',
         year: 1950
     })
-    const car = await client.hGetAll('car')
-    if (Object.keys(car).length === 0) {
-        console.log('Car not found, respond 404')
-    }
-    console.log(car)
+    await client.hSet('car#2', {
+        color: 'red',
+        year: 1950
+    })
+    await client.hSet('car#3', {
+        color: 'red',
+        year: 1950
+    })
+    const commands = [1,2,3].map((id) => {
+        return client.hGetAll(`car#${id}`)
+    })
+    const results = await Promise.all(commands)
+    console.log(results)
+    // const car = await client.hGetAll('car')
+    // if (Object.keys(car).length === 0) {
+    //     console.log('Car not found, respond 404')
+    // }
+    // console.log(car)
 }
 run();
