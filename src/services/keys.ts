12521d5f63c8acf169f7a08295f882554e83f965
william <william@magicmanager.nl>
2022-06-20 16:41:35 +0200

    Adding a Like System

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -3,3 +3,4 @@ export const usersKey = (id:string) => `users#${id}`
 export const sessionsKey = (id:string) => `sessions#${id}`
 export const itemsKey = (id:string) => `items#${id}`
 export const usernamesUniqueKey = () => `usernames:unique`
+export const userLikesKey = (id:string) => `users:likes${id}`
--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,6 +1,13 @@
 import type { CreateItemAttrs } from '$services/types';
+import {DateTime} from "luxon";
 
 export const serialize = (attrs: CreateItemAttrs) => {
+    if (!attrs.createdAt) {
+        attrs.createdAt = DateTime.now()
+    }
+    if (!attrs.endingAt) {
+        attrs.endingAt = DateTime.now()
+    }
     const { createdAt, endingAt } = attrs
 
     return { ...attrs, createdAt: createdAt.toMillis(), endingAt:endingAt.toMillis() }
