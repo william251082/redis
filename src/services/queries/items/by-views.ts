1b26f756ddbc61cabde735db124ecff5e6a33b44
william <william@magicmanager.nl>
2022-08-09 17:18:39 +0200

    Joining Data with Sort

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,16 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import {client} from "$services/redis";
+import {itemsByViewsKey, itemsKey} from "$services/keys";
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+    const results = await client.sort(
+        itemsByViewsKey(), {
+            GET: [
+                '#',
+                `${itemsKey('*')}->name`,
+                `${itemsKey('*')}->views`
+            ],
+            BY: 'score' // disable sorting
+        }
+    )
+    console.log(results)
+}
