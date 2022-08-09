ea3a236763621bae79a4a2d73d04a5206554c1fb
william <william@magicmanager.nl>
2022-08-09 14:20:37 +0200

    Items by Ending Soonest

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,15 @@
+import {client} from "$services/redis";
+import {itemsEndingAtKey} from "$services/keys";
+
 export const itemsByEndingTime = async (
 	order: 'DESC' | 'ASC' = 'DESC',
 	offset = 0,
 	count = 10
-) => {};
+) => {
+	const ids = await client.zRange(itemsEndingAtKey(), Date.now(), '+inf', {
+			BY: 'SCORE',
+			LIMIT: { offset, count }
+		}
+	)
+	console.log(ids)
+}
