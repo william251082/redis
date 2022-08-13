f5d4c2b512b6197467612720faeaf350a8c8a24d
william <william@magicmanager.nl>
2022-08-13 14:48:05 +0200

    Executing the Search

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,3 +1,7 @@
+import {client} from "$services/redis";
+import {itemsIndexKey} from "$services/keys";
+import {deserialize} from "$services/queries/items/deserialize";
+
 export const searchItems = async (term: string, size: number = 5) => {
     const cleaned = term
         .replaceAll(/^a-zA-Z0-9/g, '')
@@ -5,4 +9,23 @@ export const searchItems = async (term: string, size: number = 5) => {
         .split(' ')
         .map((word) => word ? `%${word}%` : '')
         .join(' ')
+
+    // Look at cleaned and make sure it is valid
+    if (cleaned === '') {
+        return []
+    }
+
+    // Use the client to do an actual search
+    const results = await client.ft.search(
+        itemsIndexKey(),
+        cleaned, {
+            LIMIT: {
+                from : 0,
+                size
+            }
+        }
+    )
+
+    // Deserialize and return the search results
+    return results.documents.map(({id, value}) => deserialize(id, value as any))
 }
