e3889cbd2eb77f6ce38a5145679632c2fcc924fc
william <william@magicmanager.nl>
2022-08-13 14:30:54 +0200

    Parsing the Search Term

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,8 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+export const searchItems = async (term: string, size: number = 5) => {
+    const cleaned = term
+        .replaceAll(/^a-zA-Z0-9/g, '')
+        .trim()
+        .split(' ')
+        .map((word) => word ? `%${word}%` : '')
+        .join(' ')
+}
