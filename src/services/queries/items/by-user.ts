fea1516512b10124ae06879784f3ec012e8dfed9
william <william@magicmanager.nl>
2022-08-13 16:15:03 +0200

    Executing the Search

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -1,3 +1,7 @@
+import {client} from "$services/redis";
+import {itemsIndexKey} from "$services/keys";
+import {deserialize} from "$services/queries/items/deserialize";
+
 interface QueryOpts {
 	page: number;
 	perPage: number;
@@ -6,4 +10,25 @@ interface QueryOpts {
 	tag: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+export const itemsByUser = async (userId: string, opts: QueryOpts) => {
+	const { page, perPage, sortBy, direction } = opts
+	const query = `@ownerId:{${userId}}`
+	const sortCriteria = sortBy && direction && {
+		BY: sortBy, DIRECTION: direction
+	}
+	const { total, documents } = await client.ft.search(itemsIndexKey(), query, {
+		ON: 'HASH',
+		SORTBY: sortCriteria,
+		LIMIT: {
+			from: page * perPage,
+			size: perPage
+		}
+	} as any);
+
+	return {
+		totalPages: Math.ceil(total / perPage),
+		items: documents.map(({ id, value }) => {
+			return deserialize(id.replace('items#', ''), value as any)
+		})
+	}
+}
